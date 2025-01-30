13d8fbd9e851f1921b9e74bb255cb60a286a50e0
drnkwtr <drnkwtr@forblitz.ru>
2025-01-31 01:48:09 +0300

    LATEST try to fix make file?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies('83.147.247.64');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
